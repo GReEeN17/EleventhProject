285cf92905c66a3f9539a43048a76e773913e045
Aleksandr <azelen04@mail.ru>
2024-02-24 16:00:01 +0300

    refactored jwt options

--- Server/EleventhProject.Server.Application/Middlewares/JwtOptions.cs
+++ Server/EleventhProject.Server.Application/Middlewares/JwtOptions.cs
@@ -2,6 +2,6 @@ namespace EleventhProject.Server.Application.Middlewares;
 
 public class JwtOptions
 {
-    public string SecretKey { get; set; }
-    public int ExpiresHours { get; set; }
+    public string SecretKey { get; set; } = "Eleventh_will_win_this_hakathon_do_not_doubt_about_this";
+    public int ExpiresHours { get; set; } = 12;
 }
