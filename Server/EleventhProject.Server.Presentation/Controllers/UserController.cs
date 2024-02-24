fc625254f4d7a94b6619a6ef2359b3d0d8f5aee6
marcelo <khmarcelo044@gmail.com>
2024-02-24 13:46:14 +0300

    create user endpoint

--- Server/EleventhProject.Server.Presentation/Controllers/UserController.cs
+++ Server/EleventhProject.Server.Presentation/Controllers/UserController.cs
@@ -24,4 +24,24 @@ public class UserController(IUserService userService) : BaseController
             return StatusCode(500, $"Internal server error: {ex.Message}");
         }
     }
+    
+    [HttpPost("user/createUser")]
+    public async Task<IActionResult> CreateUser(int cityId, string username, string password, 
+        long phoneNumber, string surname, string name, string middleName)
+    {
+        try
+        {
+            var user = await userService.CreateUser(cityId, username, password, phoneNumber, surname, name, middleName);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex.Message}");
+        }
+    }
 }
